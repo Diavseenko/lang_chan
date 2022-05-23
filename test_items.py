ef53152673eb7711b4eb63ba6e13549b8ffef098
DmitrY <diavseenko@yandex.ru>
2022-05-24 01:50:35 +0300

    check logic 5,6

--- test_items.py
+++ test_items.py
@@ -2,4 +2,7 @@ def test_check_add_to_cart(browser):
     button = browser.find_element_by_class_name("btn-add-to-basket")
     button.click()
 
+    message = browser.find_element_by_class_name("alertinner ")
+    assert message.text == "Coders at Work has been added to your basket.", "Its not added in cart"
+
 
