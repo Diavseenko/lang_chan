4de971563f43b9230562271c09997bfe5f217e6a
DmitrY <diavseenko@yandex.ru>
2022-05-24 01:44:49 +0300

    logic for use cmdopt

--- conftest.py
+++ conftest.py
@@ -1,11 +1,15 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 @pytest.fixture()
-def browser():
+def browser(cmdopt):
     print("\nstart browser..")
-    browser = webdriver.Chrome()
+    link = f"http://selenium1py.pythonanywhere.com/{cmdopt}/catalogue/coders-at-work_207/"
+    browser = webdriver.Chrome(link)
     yield browser
     print("\nclose browser..")
     browser.quit()
+
+
 
