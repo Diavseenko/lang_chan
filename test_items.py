9875f84d4e81118a3777f99b1dca8169eec83083
DmitrY <diavseenko@yandex.ru>
2022-05-24 14:54:08 +0300

    last version

--- test_items.py
+++ test_items.py
@@ -3,6 +3,7 @@ def test_check_add_to_cart(browser):
     button.click()
 
     message = browser.find_element_by_class_name("alertinner ")
-    assert message.text == "Coders at Work has been added to your basket.", "Its not added in cart"
+    print("Coders at Work" in message.text)
+    assert "Coders at Work" in message.text, "Its not added in cart"
 
 
