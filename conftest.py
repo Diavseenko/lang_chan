59879fa787d37024853d2eb69124135be8b75ad5
DmitrY <diavseenko@yandex.ru>
2022-05-24 14:54:02 +0300

    last version

--- conftest.py
+++ conftest.py
@@ -1,15 +1,38 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 
-@pytest.fixture()
-def browser(cmdopt):
-    print("\nstart browser..")
-    link = f"http://selenium1py.pythonanywhere.com/{cmdopt}/catalogue/coders-at-work_207/"
-    browser = webdriver.Chrome(link)
+def pytest_addoption(parser):
+    parser.addoption(
+        "--language", action="store", default="None", help="Chooice some language"
+    )
+
+@pytest.fixture(scope="function")
+def browser(request):
+    browser_lang = request.config.getoption("--language")
+    browser = None
+    browser = webdriver.Chrome()
+    browser.implicitly_wait(15)
+    link1 = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/"
+    link2 = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
+    if browser_lang == "en":
+        print("\nstart chrome browser for test..")
+        browser.get(link1)
+
+    elif browser_lang == "ru":
+        print("\nstart chrome browser for test..")
+        browser.get(link2)
+
+    else:
+        raise pytest.UsageError("check your language")
     yield browser
-    print("\nclose browser..")
+    print("\nquit browser..")
     browser.quit()
 
 
-
+#@pytest.fixture()
+#def browser(request):
+#    print("\nstart browser..")
+#    browser = webdriver.Chrome()
+#    yield browser
+#    print("\nclose browser..")
+#    browser.quit()
